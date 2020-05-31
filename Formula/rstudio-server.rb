7f6064376524f52a4deaee9f10f12a7b0fcb99e6
Jonathan Keane <jkeane@gmail.com>
2020-05-31 14:59:56 -0500

    Update rstudio-server.rb

--- Formula/rstudio-server.rb
+++ Formula/rstudio-server.rb
@@ -4,13 +4,6 @@ class RstudioServer < Formula
   url "https://github.com/rstudio/rstudio/archive/v1.2.5001.tar.gz"
   sha256 "0d1ec7aef62bda1ceec364e372fdbbcc4da502a3f03eddcddc700bdead6ee840"
 
-  bottle do
-    root_url "https://linuxbrew.bintray.com/bottles-base"
-    cellar :any
-    sha256 "255ef12e823fc4f2a3e4c3f673cda58cedbd70e15a002ea63d8921a1fb839a85" => :mojave
-    sha256 "6326a328ed08563c3ce10624b3a868b03a205bea1b7312baa13c321cbbb10d2a" => :x86_64_linux
-  end
-
   if OS.linux?
     depends_on "patchelf" => :build
     depends_on "libedit"
