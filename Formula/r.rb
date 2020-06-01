02ce306d4da7aa6b86c501637d8b1d7a83c67f17
Jonathan Keane <jkeane@gmail.com>
2020-06-01 18:39:33 -0500

    Add r?

---
+++ Formula/r.rb
@@ -0,0 +1,126 @@
+class R < Formula
+  desc "Software environment for statistical computing"
+  homepage "https://www.r-project.org/"
+  url "https://cran.r-project.org/src/base/R-4/R-4.0.0.tar.gz"
+  sha256 "06beb0291b569978484eb0dcb5d2339665ec745737bdfb4e873e7a5a75492940"
+  revision 1
+
+  bottle do
+    rebuild 1
+    sha256 "cff148724950c35ef1f42450259ea2775e82101af114fd306dc20df04a9d13c0" => :catalina
+    sha256 "55bf4a20c65107934cad232c4e031d88920bb7e57d4b044350c0109899f53fcc" => :mojave
+    sha256 "1ebe182e8e6dde809cbb181a63a395d906ee0ea326bb80b432ecebacbea8b889" => :high_sierra
+    sha256 "320a0f0a2a93c118e8662fcc5fe36c187bf1e00f2b6cc1475a07ff20dd0c0c54" => :x86_64_linux
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gcc" # for gfortran
+  depends_on "gettext"
+  depends_on "jpeg"
+  depends_on "libpng"
+  depends_on "openblas"
+  depends_on "pcre2"
+  depends_on "readline"
+  depends_on "xz"
+
+  unless OS.mac?
+    depends_on "cairo"
+    depends_on "curl"
+    depends_on "pango"
+    depends_on "linuxbrew/xorg/xorg"
+  end
+
+  # needed to preserve executable permissions on files without shebangs
+  skip_clean "lib/R/bin", "lib/R/doc"
+
+  def install
+    # Fix dyld: lazy symbol binding failed: Symbol not found: _clock_gettime
+    if MacOS.version == "10.11" && MacOS::Xcode.installed? &&
+       MacOS::Xcode.version >= "8.0"
+      ENV["ac_cv_have_decl_clock_gettime"] = "no"
+    end
+
+    args = [
+      "--prefix=#{prefix}",
+      "--enable-memory-profiling",
+      "--with-lapack",
+      "--enable-R-shlib",
+      "--disable-java",
+      "--with-blas=-L#{Formula["openblas"].opt_lib} -lopenblas",
+    ]
+
+    # don't remember Homebrew's sed shim
+    args << "SED=/usr/bin/sed" if File.exist?("/usr/bin/sed")
+
+    if OS.mac?
+      args << "--without-cairo"
+      args << "--without-tcltk"
+      args << "--without-x"
+      args << "--with-aqua"
+    end
+
+    unless OS.mac?
+      args << "--libdir=#{lib}" # avoid using lib64 on CentOS
+      args << "--with-cairo"
+
+      # If LDFLAGS contains any -L options, configure sets LD_LIBRARY_PATH to
+      # search those directories. Remove -LHOMEBREW_PREFIX/lib from LDFLAGS.
+      ENV.remove "LDFLAGS", "-L#{HOMEBREW_PREFIX}/lib"
+    end
+
+    # Help CRAN packages find gettext and readline
+    ["gettext", "readline"].each do |f|
+      ENV.append "CPPFLAGS", "-I#{Formula[f].opt_include}"
+      ENV.append "LDFLAGS", "-L#{Formula[f].opt_lib}"
+    end
+
+    system "./configure", *args
+    system "make"
+    ENV.deparallelize do
+      system "make", "install"
+    end
+
+    cd "src/nmath/standalone" do
+      system "make"
+      ENV.deparallelize do
+        system "make", "install"
+      end
+    end
+
+    r_home = lib/"R"
+
+    # make Homebrew packages discoverable for R CMD INSTALL
+    inreplace r_home/"etc/Makeconf" do |s|
+      s.gsub!(/^CPPFLAGS =.*/, "\\0 -I#{HOMEBREW_PREFIX}/include")
+      s.gsub!(/^LDFLAGS =.*/, "\\0 -L#{HOMEBREW_PREFIX}/lib")
+      s.gsub!(/.LDFLAGS =.*/, "\\0 $(LDFLAGS)")
+    end
+
+    include.install_symlink Dir[r_home/"include/*"]
+    lib.install_symlink Dir[r_home/"lib/*"]
+
+    # avoid triggering mandatory rebuilds of r when gcc is upgraded
+    inreplace lib/"R/etc/Makeconf",
+      Formula["gcc"].prefix.realpath,
+      Formula["gcc"].opt_prefix,
+      OS.mac?
+  end
+
+  def post_install
+    short_version =
+      `#{bin}/Rscript -e 'cat(as.character(getRversion()[1,1:2]))'`.strip
+    site_library = HOMEBREW_PREFIX/"lib/R/#{short_version}/site-library"
+    site_library.mkpath
+    ln_s site_library, lib/"R/site-library"
+  end
+
+  test do
+    dylib_ext = OS.mac? ? ".dylib" : ".so"
+    assert_equal "[1] 2", shell_output("#{bin}/Rscript -e 'print(1+1)'").chomp
+    assert_equal dylib_ext, shell_output("#{bin}/R CMD config DYLIB_EXT").chomp
+
+    system bin/"Rscript -e \'install.packages(\"gss\", \".\", \"https://cloud.r-project.org\")\'"
+    assert_predicate testpath/"gss/libs/gss.so", :exist?,
+                     "Failed to install gss package"
+  end
+end
